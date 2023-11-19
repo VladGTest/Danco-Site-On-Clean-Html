ccd141f4d13918e68ec07d1d41899bac0d8b0565
VladGTest <vladislavfordev@gmail.com>
2023-11-19 17:51:10 +0100

    Add changes to 19.11.2023, 17:51

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const projectsFront = document.querySelectorAll('.projects__item-front');
 const projectsBack = document.querySelectorAll('.projects__item-back');
 const projectsItem = document.querySelectorAll('.projects__item');
+const hamburger = document.querySelector('.hamburger');
+const headerMenu = document.querySelector('.header__list');
+
+
+hamburger.addEventListener('click', () => {
+    headerMenu.classList.toggle('active');
+    hamburger.classList.toggle('active');
+    
+    if(headerMenu.classList.contains('active'))
+        document.body.style.overflow = "hidden";
+    else
+        document.body.style.overflow = "auto";
+
+});
 
 for(let i = 0; i < projectsItem.length;i++){
     projectsItem[i].addEventListener('mouseover', () => {
@@ -16,3 +30,5 @@ for(let i = 0; i < projectsItem.length;i++){
         projectsBack[i].classList.remove('active');
     });
 }
+
+

8705f5b93876d76de97df86d957b35825516ae79
VladGTest <vladislavfordev@gmail.com>
2023-12-05 15:47:10 +0100

    Add changes on 05.12.2023, 15:47

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const projectsBack = document.querySelectorAll('.projects__item-back');
 const projectsItem = document.querySelectorAll('.projects__item');
 const hamburger = document.querySelector('.hamburger');
 const headerMenu = document.querySelector('.header__list');
+const btnCall = document.querySelector('.careers__call-btn');
+const svgFromBtnCall = document.querySelector('.headerHeart');
 
 
 hamburger.addEventListener('click', () => {
@@ -30,5 +32,19 @@ for(let i = 0; i < projectsItem.length;i++){
         projectsBack[i].classList.remove('active');
     });
 }
+
+$(document).ready(function(){
+    $('.roofing-services__slider-carousel').slick({
+        dots: true,
+        arrows: false,
+        variableWidth: true,
+        slidesPerRow: 1,
+                rows: 1
+    });
+})
+
+btnCall.addEventListener('mouseenter', () => {
+    svgFromBtnCall.style.fill = "white";
+});
 
 

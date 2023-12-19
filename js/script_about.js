1ca78832cd83fe9f8efd3f308779f3902fc91bd7
VladGTest <vladislavfordev@gmail.com>
2023-12-19 16:08:49 +0100

    Add careers page and changes for other elements on 16:08, 19.12.2023

--- js/script_about.js
+++ js/script_about.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector('.hamburger');
 const headerMenu = document.querySelector('.header__list');
 const teamItems = document.querySelectorAll('.team__item');
-const teamItemsDescr = document.querySelectorAll('.team__item-descr-descr');
+let teamItemsDescr = document.querySelectorAll('.team__item-descr');
+const teamItemsDescrInfo = document.querySelectorAll('.team__item-descr-descr');
+
 
 hamburger.addEventListener('click', () => {
     headerMenu.classList.toggle('active');
@@ -17,11 +19,13 @@ hamburger.addEventListener('click', () => {
 for(let i = 0; i < teamItems.length;i++){
     teamItems[i].addEventListener('mouseover',()=>{
         teamItemsDescr[i].classList.add('active');
+        teamItemsDescrInfo[i].classList.add('active');
     })
 }
 
 for(let i = 0; i < teamItems.length;i++){
     teamItems[i].addEventListener('mouseout',()=>{
         teamItemsDescr[i].classList.remove('active');
+        teamItemsDescrInfo[i].classList.remove('active');
     })
 }
